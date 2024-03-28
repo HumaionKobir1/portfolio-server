9716b187506a7ca2df11c546d2f57eee42312a90
Humaion Kobir <huamionkobir341@gmail.com>
2024-03-28 12:31:12 +0600

    get data and get specfic id

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
-// middleWare
-app.use(cors());
-app.use(express.json());
+// middleware
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+  }
+  app.use(cors(corsOptions))
+  app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4hywmoi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -31,6 +36,13 @@ async function run() {
     
     app.get('/projects', async(req, res) => {
         const result = await projectCollection.find().toArray();
+        res.send(result);
+    })
+
+    app.get('/projects/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await projectCollection.findOne(query);
         res.send(result);
     })
 
